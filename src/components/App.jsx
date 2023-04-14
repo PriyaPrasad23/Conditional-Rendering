75a8feda8da19d694a406d00841232da2aed29d3
PriyaPrasad23 <128690245+PriyaPrasad23@users.noreply.github.com>
2023-04-14 16:24:42 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+function check() {
+  if (isLoggedIn === true) return <h1>Hello</h1>;
+  else {
+    return <Login />;
+  }
+}
+
+function App() {
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
+}
+export default App;
